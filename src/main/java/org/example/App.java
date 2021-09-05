b84e62d9d8eab318b97360975addcd9da17545e3
Ryan <rdturner40@gmail.com>
2021-09-05 12:16:32 -0400

    Init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Ryan Turner
+ */
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
